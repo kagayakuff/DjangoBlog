5e22967124fc11b3b0d05aa4e8e6efbe2044ed61
luojihui <luojihui@hexin.im>
2020-05-18 17:39:34 +0800

    update config

--- bin/django_start.sh
+++ bin/django_start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 NAME="djangoblog" # Name of the application
-DJANGODIR=/var/www/DjangoBlog # Django project directory
+DJANGODIR=/home/ubuntu/python/DjangoBlog # Django project directory
 SOCKFILE=/var/www/DjangoBlog/run/gunicorn.sock # we will communicte using this unix socket
-USER=root # the user to run as
-GROUP=root # the group to run as
-NUM_WORKERS=3 # how many worker processes should Gunicorn spawn
+USER=ubuntu # the user to run as
+GROUP=ubuntu # the group to run as
+NUM_WORKERS=1 # how many worker processes should Gunicorn spawn
 DJANGO_SETTINGS_MODULE=DjangoBlog.settings # which settings file should Django use
 DJANGO_WSGI_MODULE=DjangoBlog.wsgi # WSGI module name
 
@@ -23,7 +23,7 @@ test -d $RUNDIR || mkdir -p $RUNDIR
 
 # Start your Django Unicorn
 # Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
-exec /var/www/dev/python3/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
+exec /var/www/dev/python3/bin/gunicorn ${DJANGO_WSGI_MODULE}:application -b 0.0.0.0:8888 \
 --name $NAME \
 --workers $NUM_WORKERS \
 --user=$USER --group=$GROUP \
