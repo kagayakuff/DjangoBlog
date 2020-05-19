38668e113cc0578bf79198c5dc38c8d83e8d8492
luojihui <luojihui@hexin.im>
2020-05-19 14:03:26 +0800

    setting

--- DjangoBlog/utils.py
+++ DjangoBlog/utils.py
@@ -195,7 +195,7 @@ def get_blog_setting():
         from blog.models import BlogSettings
         if not BlogSettings.objects.count():
             setting = BlogSettings()
-            setting.sitename = 'DjangoBlog'
+            setting.sitename = '华泰辉创'
             setting.site_description = '基于Django的博客系统'
             setting.site_seo_description = '基于Django的博客系统'
             setting.site_keywords = 'Django,Python'
